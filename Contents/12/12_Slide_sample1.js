616d3c575d8cba6cdba167d787d4b2308d92c457
Bagasanchi <bagaezwin@gmail.com>
2025-11-17 08:08:41 +0800

    12

--- Contents/12/12_Slide_sample1.js
+++ Contents/12/12_Slide_sample1.js
@@ -12,3 +12,11 @@ console.log(number == str);
 console.log(number === str);
 console.log(number != str);
 console.log(number !== str);
+
+let my_number = -2;
+if (!(my_number > 0 || my_number % 2 == 0 )) {
+    console.log(my_number + " is greater than 0 but is not an even number.");
+}
+else {
+    console.log(my_number + " is an even number greater than 0.");
+}
